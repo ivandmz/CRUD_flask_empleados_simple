7b32744f9b14de61b431659bae4ee6c3f5f60d74
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-07-19 01:20:57 -0300

    cree ruta delete

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
-from datetime import date, datetime
+from datetime import datetime
+import os
 
 app= Flask(__name__)
 mysql= MySQL()
@@ -10,6 +11,10 @@ app.config['MYSQL_DATABASE_HOST']= 'localhost'
 app.config['MYSQL_DATABASE_USER']= 'root'
 app.config['MYSQL_DATABASE_PASSWORD']= 'root'
 app.config['MYSQL_DATABASE_DB']= 'empleados'
+
+UPLOADS = os.path.join('uploads')
+app.config['UPLOADS'] = UPLOADS # guardamos la ruta como un valor en la app
+
 mysql.init_app(app)
 
 
@@ -48,17 +53,71 @@ def storage():
 
     
     sql= "INSERT INTO  `empleados`.`empleados` (nombre, correo, foto) VALUES (%s,%s,%s);"
-    datos= (_nombre,_correo,nuevoNombreFoto) # aca no iría nuevoNombreFoto? sep era asi nomas.. sino no le cambia el nombre a la foto  
+    datos= (_nombre,_correo,nuevoNombreFoto)
 
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql,datos)
-
     conn.commit()
 
     return redirect('/')
 
 
+@app.route('/modify/<int:id>')
+def modify(id):
+    sql = f'SELECT * FROM `empleados`.`empleados` WHERE id={id}' # puedo usar f'{}' o %s. lo q me sea mas comodo
+    conn = mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute(sql)
+    empleado= cursor.fetchone()
+    conn.commit()
+
+    return render_template('empleados/edit.html', empleado = empleado)
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre= request.form['txtNombre']
+    _correo= request.form['txtCorreo']
+    _foto= request.files['txtFoto']
+    id= request.form['txtId']
+
+    datos = (_nombre, _correo, id)
+
+    conn= mysql.connect()
+    cursor= conn.cursor()
+
+    if _foto.filename != '':
+        now = datetime.now()
+        tiempo= now.strftime("%Y%H%M%S")
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+    
+    sql= f'SELECT foto FROM `empleados`.`empleados` WHERE id={id}'
+    cursor.execute(sql)
+
+    nombreFoto = cursor.fetchone()[0] # esto esta muy feo
+
+    os.remove(os.path.join(app.config['UPLOADS'], nombreFoto))
+
+    conn= mysql.connect()
+    cursor=conn.cursor()
+
+    sql= f'UPDATE `empleados`.`empleados` SET nombre={_nombre}, correo={_correo} WHERE id={id}'
+    cursor.execute(sql)
+    conn.commit()
+
+
+@app.route('/delete/<int:id>') # lo que está entre <> es un parámetro.
+def delete(id):
+    # sql = "DELETE FROM `empleados`.`empleados` WHERE id=%s"
+    sql = f'DELETE FROM `empleados`.`empleados` WHERE id={id}'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    # cursor.execute(sql,id)
+    cursor.execute(sql)
+    conn.commit()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
