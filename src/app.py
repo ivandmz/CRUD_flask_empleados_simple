c94277409dab125ccc49abc1ff552f92658ebdec
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-07-20 01:45:11 -0300

    arreg mets, fn querymysql todo %s

--- src/app.py
+++ src/app.py
@@ -18,6 +18,26 @@ app.config['UPLOADS'] = UPLOADS # guardamos la ruta como un valor en la app
 
 mysql.init_app(app)
 
+conn=mysql.connect() # como tengo estos aca arriba puedo borrar los de los metodos debajo. (pero solo los voy a comentar)
+cursor=conn.cursor()
+
+def queryMySql(query,data = None, tipoDeRetorno = 'none'):
+    if data != None:
+        cursor.execute(query,data)
+    else:
+        cursor.execute(query)
+
+    if tipoDeRetorno == "one":
+        registro = cursor.fetchone()
+        conn.commit()
+        return registro # no es buena practica usar el return dentro y mas de una vez
+    elif tipoDeRetorno == "all":
+        registro = cursor.fetchall()
+        conn.commit()
+        return registro # no es buena practica usar el return dentro y mas de una vez
+    else:
+        conn.commit()
+    
 
 @app.route('/fotodeusuario/<path:nombreFoto>') # esto sirve para ocultar donde tengo mis fotos. el navegador mapea esto y le da al usuario esta dire y no la real.
 def uploads(nombreFoto):
@@ -25,15 +45,14 @@ def uploads(nombreFoto):
 
 @app.route('/') # define una ruta de acceso
 def index():
-    conn=mysql.connect()
-    cursor = conn.cursor()
+    # conn=mysql.connect()
+    # cursor = conn.cursor()
 
     sql= "SELECT * FROM `empleados`.`empleados`;"
-    cursor.execute(sql)
-    
-    empleados = cursor.fetchall()
-
-    conn.commit()
+    empleados = queryMySql(sql,None,"all")
+    # cursor.execute(sql)
+    # empleados = cursor.fetchall()
+    # conn.commit()
 
     return render_template('empleados/index.html', empleados=empleados)
 
@@ -48,9 +67,7 @@ def storage():
     _foto = request.files['txtFoto'] # lo recibe como archivo, un stream de datos
 
     now = datetime.now()
-    print(now)
     tiempo = now.strftime("%Y%H%M%S")
-    print(tiempo)
 
     if _foto.filename != '':
         nuevoNombreFoto = tiempo + '_' + _foto.filename
@@ -60,10 +77,11 @@ def storage():
     sql= "INSERT INTO  `empleados`.`empleados` (nombre, correo, foto) VALUES (%s,%s,%s);" # no debo usar format strings (f'algo "{dato}" algo') porque esto permite inyectar codigo malicioso en la BD "inyeccion SQL"
     datos= (_nombre,_correo,nuevoNombreFoto)
 
-    conn=mysql.connect()
-    cursor=conn.cursor()
-    cursor.execute(sql,datos)
-    conn.commit()
+    queryMySql(sql,datos) # con esta fn nos ahorramos todo el codigo de abajo
+    # conn=mysql.connect()
+    # cursor=conn.cursor()
+    # cursor.execute(sql,datos)
+    # conn.commit()
 
     return redirect('/')
 
@@ -71,11 +89,13 @@ def storage():
 @app.route('/modify/<int:id>')
 def modify(id):
     sql = 'SELECT * FROM `empleados`.`empleados` WHERE id=%s;'
-    conn = mysql.connect()
-    cursor= conn.cursor()
-    cursor.execute(sql,id)
-    empleado= cursor.fetchone()
-    conn.commit()
+    datos=[id]
+    empleado = queryMySql(sql,datos,"one")
+    # cursor.execute(sql,datos)
+    # conn = mysql.connect()
+    # cursor= conn.cursor()
+    # empleado= cursor.fetchone()
+    # conn.commit()
 
     return render_template('empleados/edit.html', empleado = empleado)
 
@@ -86,10 +106,8 @@ def update():
     _foto= request.files['txtFoto']
     id= request.form['txtId']
 
-    # datos = (_nombre, _correo, id) # old school
-
-    conn= mysql.connect()
-    cursor= conn.cursor()
+    # conn= mysql.connect()
+    # cursor= conn.cursor()
 
     if _foto.filename != '':
         now = datetime.now()
@@ -98,49 +116,57 @@ def update():
         _foto.save("uploads/" + nuevoNombreFoto) # "src/uploads/"
     
         sql= 'SELECT foto FROM `empleados`.`empleados` WHERE id=%s;'
-        cursor.execute(sql,id)
-        conn.commit()
+        datos = [id]
+        nombreFoto = queryMySql(sql,datos,"one")
+        # cursor.execute(sql,id)
+        # conn.commit()
+        # nombreFoto = cursor.fetchone()[0] # esto esta muy feo
 
-        nombreFoto = cursor.fetchone()[0] # esto esta muy feo
-        borrarEstaFoto = os.path.join(app.config['UPLOADS'], nombreFoto) # esto no se para que lo hace, para mi que se equivocó y esto va abajo
-        print(borrarEstaFoto)
+        borrarEstaFoto = os.path.join(app.config['UPLOADS'], nombreFoto[0]) # esto no se para que lo hace, para mi que se equivocó y esto va abajo
 
         try:
-            os.remove(os.path.join(app.config['UPLOADS'], nombreFoto)) # para mi aca va borrarEstaFoto en lugar de nombreFoto
+            os.remove(os.path.join(app.config['UPLOADS'], nombreFoto[0])) # para mi aca va borrarEstaFoto en lugar de nombreFoto
         except:
             pass
 
         sql = 'UPDATE `empleados`.`empleados` SET foto=%s WHERE id=%s;'
         datos= (nuevoNombreFoto,id)
-        cursor.execute(sql, datos)
-        conn.commit()
+        queryMySql(sql,datos)
+        # cursor.execute(sql, datos)
+        # conn.commit()
         
     sql = 'UPDATE `empleados`.`empleados` SET nombre=%s, correo=%s WHERE id=%s;'
     datos = (_nombre,_correo,id)
-    cursor.execute(sql,datos)
-    conn.commit()
+    queryMySql(sql,datos)
+    # cursor.execute(sql,datos)
+    # conn.commit()
 
     return redirect('/')
 
 @app.route('/delete/<int:id>') # lo que está entre <> es un parámetro.
 def delete(id):
-    conn = mysql.connect()
-    cursor = conn.cursor()
+    # conn = mysql.connect()
+    # cursor = conn.cursor()
 
     sql = 'SELECT foto from `empleados`.`empleados` WHERE id=%s;'
-    cursor.execute(sql,id)
+    datos = [id]
 
-    nombreFoto = cursor.fetchone()[0] # le pongo 0 para desestructurar y que me de el objeto 1 de la tupla
+    nombreFoto = queryMySql(sql,datos, "one")
+    # cursor.execute(sql,id)
+
+    # nombreFoto = cursor.fetchone()[0] # le pongo 0 para desestructurar y que me de el objeto 1 de la tupla
 
     try:
-        os.remove(os.path.join(app.config['UPLOADS'], nombreFoto))
+        os.remove(os.path.join(app.config['UPLOADS'], nombreFoto[0]))
     except:
         pass
 
     sql = 'DELETE FROM `empleados`.`empleados` WHERE id=%s;'
-    cursor.execute(sql,id) 
     
-    conn.commit()
+    queryMySql(sql,datos)
+    # cursor.execute(sql,id) 
+    
+    # conn.commit()
 
     return redirect('/')
 
