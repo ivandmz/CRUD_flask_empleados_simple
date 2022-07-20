ee09479638437035b825ce91489a41bd429428b7
Ivan Muñoz <ivandariomunioz@gmail.com>
2022-07-20 14:47:42 -0300

    unifico Met. GET POST y URI en create edit etc

--- src/app.py
+++ src/app.py
@@ -55,82 +55,123 @@ def index():
 
 
 # por defecto viene el method GET: ('/create', methods=['GET'])
-@app.route('/create')
-def create():
-    return render_template('empleados/create.html')
+@app.route('/empleado/crear', methods=['GET','POST'])
+def alta_empleado():
+    if request.method == "GET":
+        return render_template('empleados/create.html') # no es buena practica enviar returns dentro! pero el profe no lo explicó
+    elif request.method == "POST":
+        _nombre = request.form['txtNombre']  # lo recibe del form
+        _correo = request.form['txtCorreo']
+        _foto = request.files['txtFoto']# lo recibe como archivo, un stream de datos
 
-
-@app.route('/store', methods=['POST'])
-def storage():
-    _nombre = request.form['txtNombre']  # lo recibe del form
-    _correo = request.form['txtCorreo']
-    # lo recibe como archivo, un stream de datos
-    _foto = request.files['txtFoto']
-
-    now = datetime.now()
-    tiempo = now.strftime("%Y%H%M%S")
-
-    if _foto.filename != '':
-        nuevoNombreFoto = tiempo + '_' + _foto.filename
-        _foto.save("uploads/" + nuevoNombreFoto)  # "src/uploads/"
-
-    # no debo usar format strings (f'algo "{dato}" algo') porque esto permite inyectar codigo malicioso en la BD "inyeccion SQL"
-    sql = "INSERT INTO  `empleados`.`empleados` (nombre, correo, foto) VALUES (%s,%s,%s);"
-    datos = (_nombre, _correo, nuevoNombreFoto)
-
-    queryMySql(sql, datos)  # con esta fn nos ahorramos todo el codigo de abajo
-
-    return redirect('/')
-
-
-@app.route('/modify/<int:id>')
-def modify(id):
-    sql = 'SELECT * FROM `empleados`.`empleados` WHERE id=%s;'
-    datos = [id]
-    empleado = queryMySql(sql, datos, "one")
-
-    return render_template('empleados/edit.html', empleado=empleado)
-
-
-@app.route('/update', methods=['POST'])
-def update():
-    _nombre = request.form['txtNombre']
-    _correo = request.form['txtCorreo']
-    _foto = request.files['txtFoto']
-    id = request.form['txtId']
-
-    if _foto.filename != '':
         now = datetime.now()
         tiempo = now.strftime("%Y%H%M%S")
-        nuevoNombreFoto = tiempo + '_' + _foto.filename
-        _foto.save("uploads/" + nuevoNombreFoto)  # "src/uploads/"
 
-        sql = 'SELECT foto FROM `empleados`.`empleados` WHERE id=%s;'
+        if _foto.filename != '':
+            nuevoNombreFoto = tiempo + '_' + _foto.filename
+            _foto.save("uploads/" + nuevoNombreFoto)  # "src/uploads/"
+
+        # no debo usar format strings (f'algo "{dato}" algo') porque esto permite inyectar codigo malicioso en la BD "inyeccion SQL"
+        sql = "INSERT INTO  `empleados`.`empleados` (nombre, correo, foto) VALUES (%s,%s,%s);"
+        datos = (_nombre, _correo, nuevoNombreFoto)
+
+        queryMySql(sql, datos)  # con esta fn nos ahorramos todo el codigo de abajo
+
+        return redirect('/')
+
+
+# @app.route('/modify/<int:id>')
+# def modify(id):
+#     sql = 'SELECT * FROM `empleados`.`empleados` WHERE id=%s;'
+#     datos = [id]
+#     empleado = queryMySql(sql, datos, "one")
+
+#     return render_template('empleados/edit.html', empleado=empleado)
+
+
+# @app.route('/update', methods=['POST'])
+# def update():
+#     _nombre = request.form['txtNombre']
+#     _correo = request.form['txtCorreo']
+#     _foto = request.files['txtFoto']
+#     id = request.form['txtId']
+
+#     if _foto.filename != '':
+#         now = datetime.now()
+#         tiempo = now.strftime("%Y%H%M%S")
+#         nuevoNombreFoto = tiempo + '_' + _foto.filename
+#         _foto.save("uploads/" + nuevoNombreFoto)  # "src/uploads/"
+
+#         sql = 'SELECT foto FROM `empleados`.`empleados` WHERE id=%s;'
+#         datos = [id]
+#         nombreFoto = queryMySql(sql, datos, "one")
+#         print(nombreFoto)
+
+#         try:
+#             # para mi aca va borrarEstaFoto en lugar de nombreFoto
+#             os.remove(os.path.join(app.config['UPLOADS'], nombreFoto['foto']))
+#         except:
+#             print('-------------------')
+#             print('Error al intentar borrar la foto.')
+#             pass
+
+#         sql = 'UPDATE `empleados`.`empleados` SET foto=%s WHERE id=%s;'
+#         datos = (nuevoNombreFoto, id)
+#         queryMySql(sql, datos)
+
+#     sql = 'UPDATE `empleados`.`empleados` SET nombre=%s, correo=%s WHERE id=%s;'
+#     datos = (_nombre, _correo, id)
+#     queryMySql(sql, datos)
+
+#     return redirect('/')
+
+@app.route('/empleado/modificar/<int:id>', methods=['GET','POST'])
+def modif_empleado(id):
+    if request.method == "GET":
+        sql = 'SELECT * FROM `empleados`.`empleados` WHERE id=%s;'
         datos = [id]
-        nombreFoto = queryMySql(sql, datos, "one")
-        print(nombreFoto)
+        empleado = queryMySql(sql, datos, "one")
 
-        try:
-            # para mi aca va borrarEstaFoto en lugar de nombreFoto
-            os.remove(os.path.join(app.config['UPLOADS'], nombreFoto['foto']))
-        except:
-            print('-------------------')
-            print('Error al intentar borrar la foto.')
-            pass
+        return render_template('empleados/edit.html', empleado=empleado)
 
-        sql = 'UPDATE `empleados`.`empleados` SET foto=%s WHERE id=%s;'
-        datos = (nuevoNombreFoto, id)
+    elif request.method == "POST":
+        _nombre = request.form['txtNombre']
+        _correo = request.form['txtCorreo']
+        _foto = request.files['txtFoto']
+        id = request.form['txtId']
+
+        if _foto.filename != '':
+            now = datetime.now()
+            tiempo = now.strftime("%Y%H%M%S")
+            nuevoNombreFoto = tiempo + '_' + _foto.filename
+            _foto.save("uploads/" + nuevoNombreFoto)  # "src/uploads/"
+
+            sql = 'SELECT foto FROM `empleados`.`empleados` WHERE id=%s;'
+            datos = [id]
+            nombreFoto = queryMySql(sql, datos, "one")
+            print(nombreFoto)
+
+            try:
+                # para mi aca va borrarEstaFoto en lugar de nombreFoto
+                os.remove(os.path.join(app.config['UPLOADS'], nombreFoto['foto']))
+            except:
+                print('-------------------')
+                print('Error al intentar borrar la foto.')
+                pass
+
+            sql = 'UPDATE `empleados`.`empleados` SET foto=%s WHERE id=%s;'
+            datos = (nuevoNombreFoto, id)
+            queryMySql(sql, datos)
+
+        sql = 'UPDATE `empleados`.`empleados` SET nombre=%s, correo=%s WHERE id=%s;'
+        datos = (_nombre, _correo, id)
         queryMySql(sql, datos)
 
-    sql = 'UPDATE `empleados`.`empleados` SET nombre=%s, correo=%s WHERE id=%s;'
-    datos = (_nombre, _correo, id)
-    queryMySql(sql, datos)
-
-    return redirect('/')
+        return redirect('/')
 
 
-@app.route('/delete/<int:id>')  # lo que está entre <> es un parámetro.
-def delete(id):
+@app.route('/empleado/delete/<int:id>')  # lo que está entre <> es un parámetro.
+def baja_empleado(id): # lo correcto sería: en lugar de borrar, tener un casillero en la DB alta/baja. para no borrar y perder los datos.
 
     sql = 'SELECT foto from `empleados`.`empleados` WHERE id=%s;'
     datos = [id]
